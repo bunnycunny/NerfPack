47f7b01bfe07f49ef148253971eaccd7ffc962ca
Bunny_Cunny <97416657+bunnycunny@users.noreply.github.com>
2022-01-24 12:35:56 +0100

    Update skilltreetweakdata.lua

--- skilltreetweakdata.lua
+++ skilltreetweakdata.lua
@@ -1,6 +1,15 @@
-Hooks:PostHook(SkillTreeTweakData, "init", "hacker_dodge", function(self)
+Hooks:PostHook(SkillTreeTweakData, "init", "replace_skilltreetweakdata", function(self)
 	self.specializations[21][9].upgrades = {
 				"player_passive_loot_drop_multiplier",
 				"team_pocket_ecm_heal_on_kill_1"
+	self.specializations[22][3].upgrades = {
+		"player_passive_health_multiplier_3"
+	}
+	self.specializations[22][5].upgrades = {
+		"player_copr_speed_up_on_kill_1"
+	}
+	self.specializations[22][9].upgrades = {
+			"player_passive_loot_drop_multiplier",
+			"player_passive_health_multiplier_4"
 	}
 end)
