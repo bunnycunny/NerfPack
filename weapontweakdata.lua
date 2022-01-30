a42d549789afd7fef085b1147929ab8a6f424c2a
Bunny_Cunny <97416657+bunnycunny@users.noreply.github.com>
2022-01-30 14:04:43 +0100

    Update weapontweakdata.lua

--- weapontweakdata.lua
+++ weapontweakdata.lua
@@ -5,10 +5,17 @@ Hooks:PostHook(WeaponTweakData, "init", "PDTH2_WEAPONTWEAKDATA", function(self)
     local PICKUP.SNIPER_LOW_DAMAGE = 4
         
     --contractor reload speed 10% nerf
-    self.tti.timers.reload_not_empty = 2.53
-    self.tti.timers.reload_empty = 3.63
-
+    self.tti.timers = {
+		reload_not_empty = 2.53,
+		reload_empty = 3.63,
+		unequip = 0.9,
+		equip = 0.9
+	}
     --akimbo goliaths 10% reload speed nerf
-    self.x_rota.reload_not_empty = 3.3
-    self.x_rota.reload_empty = 3.85
+	self.x_rota.timers = {
+		reload_not_empty = 3.3,
+		reload_empty = 3.85,
+		unequip = 0.5,
+		equip = 0.5
+	}
 end)
