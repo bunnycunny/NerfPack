0827a98a0c556b1672708ce0173101657e0c2ad5
Bunny_Cunny <97416657+bunnycunny@users.noreply.github.com>
2022-01-30 12:10:00 +0100

    Update upgradestweakdata.lua

--- upgradestweakdata.lua
+++ upgradestweakdata.lua
@@ -1,5 +1,7 @@
 local perk_deck_skill_changes = UpgradesTweakData._init_pd2_values
 
+local perk_deck_skill_changes = UpgradesTweakData._init_pd2_values
+
 function UpgradesTweakData:_init_pd2_values()
 	perk_deck_skill_changes(self)
 		self.values.temporary.armor_break_invulnerable = {
@@ -32,5 +34,10 @@ function UpgradesTweakData:_init_pd2_values()
 		}
 end
 
-
-
+Hooks:PostHook(UpgradesTweakData, "init", "nerfpack_replace_upgradestreetweakdata", function(self)
+	self.specialization_descs[17][1].multiperk3 = 48
+	self.specialization_descs[3][7].multiperk3 = 20
+	self.specialization_descs[15][1].multiperk2 = 20
+	self.specialization_descs[19][1].multiperk1 = 50%
+	self.specialization_descs[19][1].multiperk2 = 50%
+end)
