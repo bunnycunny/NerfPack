37bcc26e462472590df9da31a645455d5c109e26
Bunny_Cunny <97416657+bunnycunny@users.noreply.github.com>
2022-01-30 12:20:32 +0100

    Update weapontweakdata.lua

--- weapontweakdata.lua
+++ weapontweakdata.lua
@@ -3,4 +3,8 @@ Hooks:PostHook(WeaponTweakData, "init", "PDTH2_WEAPONTWEAKDATA", function(self)
     local PICKUP.LMG_CAPACITY = 8
     --168 and 255 sniper rifles pickup
     local PICKUP.SNIPER_LOW_DAMAGE = 4
+        
+    --contractor reload speed 10% nerf
+    self.tti.timers.reload_empty = 3.63
+    self.tti.timers.reload_not_empty = 2.53
 end)
