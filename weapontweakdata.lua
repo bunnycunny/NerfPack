a5164ec8a7f0c741b1b28ba94bed3c1bc959e32f
Bunny_Cunny <97416657+bunnycunny@users.noreply.github.com>
2022-01-31 12:23:36 +0100

    Update weapontweakdata.lua

--- weapontweakdata.lua
+++ weapontweakdata.lua
@@ -1,18 +1,16 @@
 Hooks:PostHook(WeaponTweakData, "init", "nerfpack_weapontweakdata", function(self)
-	--contractor reload speed 10% nerf
-	self.tti.timers = {
-		reload_not_empty = 2.53,
-		reload_empty = 3.63,
-		unequip = 0.9,
-		equip = 0.9
-	}
+	--contractor reload speed 20% nerf
+   	self.tti.timers.reload_not_empty = 2.76
+	self.tti.timers.reload_empty = 3.96
+	
+	--lebensauger ammo pickup so its like all the other 168s and total capacity nerf
+	self.wa2000.AMMO_MAX = 45
+	self.wa2000.AMMO_PICKUP = { 2.70, 4.05 }
+
 	--akimbo goliaths 10% reload speed nerf
-	self.x_rota.timers = {
-		reload_not_empty = 3.3,
-		reload_empty = 3.85,
-		unequip = 0.5,
-		equip = 0.5
-	}
+	self.x_rota.timers.reload_not_empty = 3.3
+	self.x_rota.timers.reload_empty = 3.85
+	
 	--5/7 rof nerf
 	self.lemming.fire_mode_data = {
 		fire_rate = 0.08
@@ -20,4 +18,17 @@ Hooks:PostHook(WeaponTweakData, "init", "nerfpack_weapontweakdata", function(sel
 	self.lemming.single = {
 		fire_rate = 0.08
 	}
+	
+	-- +0.5s reload for compact gl
+	self.slap.timers.reload_not_empty = 3
+		
+	--lmg ammo pickup nerfs -10% for all
+	self.mg42.AMMO_PICKUP = { 10.125, 12.15 }
+	self.m249.AMMO_PICKUP = { 9, 10.8 }
+	self.par.AMMO_PICKUP = { 9, 10.8 }
+	self.rpk.AMMO_PICKUP = { 6.75, 9.45 }
+	self.m60.AMMO_PICKUP = { 6.75, 9.45 }
+	self.hk21.AMMO_PICKUP = { 6.75, 9.45 }
+	
+	
 end)
