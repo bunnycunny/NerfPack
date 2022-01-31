3c89015caac846740b3f4b97f4d30f009df86298
Bunny_Cunny <97416657+bunnycunny@users.noreply.github.com>
2022-01-31 12:14:02 +0100

    Update weapontweakdata.lua

--- weapontweakdata.lua
+++ weapontweakdata.lua
@@ -1,16 +1,23 @@
 Hooks:PostHook(WeaponTweakData, "init", "nerfpack_weapontweakdata", function(self)
-    --contractor reload speed 10% nerf
-    self.tti.timers = {
+	--contractor reload speed 10% nerf
+	self.tti.timers = {
 		reload_not_empty = 2.53,
 		reload_empty = 3.63,
 		unequip = 0.9,
 		equip = 0.9
 	}
-    --akimbo goliaths 10% reload speed nerf
+	--akimbo goliaths 10% reload speed nerf
 	self.x_rota.timers = {
 		reload_not_empty = 3.3,
 		reload_empty = 3.85,
 		unequip = 0.5,
 		equip = 0.5
+	}
+	--5/7 rof nerf
+	self.lemming.fire_mode_data = {
+		fire_rate = 0.08
+	}
+	self.lemming.single = {
+		fire_rate = 0.08
 	}
 end)
