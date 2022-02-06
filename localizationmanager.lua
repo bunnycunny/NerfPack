79924bfb676d46b24555f8fb35d3e7a985f9c15f
Bunny_Cunny <97416657+bunnycunny@users.noreply.github.com>
2022-02-06 13:22:07 +0100

    Update localizationmanager.lua

--- localizationmanager.lua
+++ localizationmanager.lua
@@ -29,20 +29,18 @@ Hooks:Add("LocalizationManagerPostInit", "nerfpack_replace_localizationmanager",
 		{
 			menu_deck22_3 = "Indomitable",
 			["menu_deck22_3_desc"] = "Your maximum health is increased by ##20%##."
+		} 
+	)
+	loc:add_localized_strings( 
+		{
+        		menu_deck22_5 = "Stalwart",
+        		["menu_deck22_5_desc"] = "Killing an enemy reduces the cooldown of Leech Ampule by ##1## second(s)."
     		} 
 	)
-	loc:add_localized_strings( 
-		{
-			menu_deck22_5 = "Stalwart",
-			["menu_deck22_5_desc"] = "Killing an enemy reduces the cooldown of Leech Ampule by ##1## second(s)."
-    
-		} 
-	)
-	loc:add_localized_strings( 
+	loc:add_localized_strings(
 		{
 			menu_deck22_9 = "Symbiosis",
-			["menu_deck22_9_desc"] = "Your maximum health is increased by ##60%##.\n\nDeck Completion Bonus: Your chance of getting a higher quality item during Payday is increased by ##10%##."
-   
-		} 
+			["menu_deck22_9_desc"] = "While the Leech Ampule is active your health is now divided into segments of ##10%##. Your maximum health is increased by ##60%##.\n\nDeck Completion Bonus: Your chance of getting a higher quality item during Payday is increased by ##10%##."
+		}
 	)
 end )
