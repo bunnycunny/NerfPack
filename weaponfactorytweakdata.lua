413743b3e5a73a456761c9b5f027c16e2c11ded4
Bunny_Cunny <97416657+bunnycunny@users.noreply.github.com>
2022-02-07 06:21:39 +0100

    Update weaponfactorytweakdata.lua

--- weaponfactorytweakdata.lua
+++ weaponfactorytweakdata.lua
@@ -1,10 +1,10 @@
 Hooks:PostHook( WeaponFactoryTweakData, "init", "nerfpack_replace_weaponfactorytweakdata", function(self)
     --speedpulls
-	self.parts.wpn_fps_m4_upg_m_quick.stats.reload = 5
-    self.parts.wpn_fps_upg_ak_m_quick.stats.reload = 5
-    self.parts.wpn_fps_ass_g36_m_quick.stats.reload = 5
-    self.parts.wpn_fps_ass_aug_m_quick.stats.reload = 5
-    self.parts.wpn_fps_smg_sr2_m_quick.stats.reload = 5
-    self.parts.wpn_fps_smg_mac10_m_quick.stats.reload = 5
-    self.parts.wpn_fps_smg_p90_m_strap.stats.reload = 5
+	self.parts.wpn_fps_m4_upg_m_quick.stats.reload = 7
+    self.parts.wpn_fps_upg_ak_m_quick.stats.reload = 7
+    self.parts.wpn_fps_ass_g36_m_quick.stats.reload = 7
+    self.parts.wpn_fps_ass_aug_m_quick.stats.reload = 7
+    self.parts.wpn_fps_smg_sr2_m_quick.stats.reload = 7
+    self.parts.wpn_fps_smg_mac10_m_quick.stats.reload = 7
+    self.parts.wpn_fps_smg_p90_m_strap.stats.reload = 7
 end )
