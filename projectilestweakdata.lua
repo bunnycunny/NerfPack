352c8892d9824d7718f7e7fb7b74483c72bcf006
Bunny_Cunny <97416657+bunnycunny@users.noreply.github.com>
2022-02-07 05:01:28 +0100

    Update projectilestweakdata.lua

--- projectilestweakdata.lua
+++ projectilestweakdata.lua
@@ -1,3 +1,3 @@
 Hooks:PostHook(ProjectilesTweakData, "init", "nerfpack_replace_projectilestweakdata", function(self)
-		self.projectiles.chico_injector.base_cooldown = 48
+		self.projectiles.chico_injector.base_cooldown = 36
 end )
